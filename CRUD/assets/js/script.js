39e3614420f7f8229bb90f9eef581195cb594545
joutria <joutria@gmail.com>
2021-06-15 17:25:50 -0500

    final features for the CRUD, including local storage
    I added the feature to use local storage and some linear gradients

--- CRUD/assets/js/script.js
+++ CRUD/assets/js/script.js
@@ -1,17 +1,17 @@
-/* here I have the prototype*/
+/* here I have the prototype */
 let user = [{
-    name: 'Jose',
-    lastName: 'Utria',
-    email: 'joutria@academlo.com'
+    name: 'Example',
+    lastName: 'User',
+    email: 'eu@example.com'
     },
     {
-        name: 'Luis',
-        lastName: 'Utria',
-        email: 'lutria@academlo.com'
+        name: 'User',
+        lastName: 'Example',
+        email: 'ue@example.com'
     }
 ];
 
-/* Here I have the identifiers*/
+/* Here I have the identifiers */
 let sname = document.getElementById('name'),
     lastname = document.getElementById('lastname'),
     email = document.getElementById('email'),
@@ -20,23 +20,34 @@ let sname = document.getElementById('name'),
     header = document.getElementById('header'),
     info=[sname,lastname,email];
 
-/* Some useful variables for the edit and delete buttons*/
+/* Some useful variables for the edit and delete buttons */
 let deleter, index;
 let editing=false;
+
+/* Local storage */
+let local = window.localStorage;
+
+if(local.length == 0){
+    console.log(local);
+    local.setItem('user', JSON.stringify(user));
+    console.log('example data set');
+}
+
+data = JSON.parse(local.getItem('user'));
 
 /* Load the table when the page loads */
 window.onload = function() {
     rendertable();
 };
 
-/* A function that iterates through the prototype array and adds the information in the table*/
+/* A function that iterates through the prototype array and adds the information in the table */
 function rendertable(){
     let row;
-    for(let i=0; i<user.length; i++){
+    for(let i=0; i<data.length; i++){
         row=table.insertRow(i+1);
         let x=0;
-        for(let j in user[i]){
-            row.insertCell(x).innerHTML = user[i][j];
+        for(let j in data[i]){
+            row.insertCell(x).innerHTML = data[i][j];
             x++;
         }
         row.insertCell(3).innerHTML="<button class='edit'>Edit</button><button class='delete'>Delete</button>";
@@ -44,21 +55,23 @@ function rendertable(){
     loadbtn();
 }
 
-/* Whit this one I clear the information befor reloading it with rendertable (just to avoid stacking)*/
+/* Whit this one I clear the information befor reloading it with rendertable (just to avoid stacking) */
 function clear(){
     table.innerHTML='';
     table.appendChild(header);
 }
 
-/* Adding functionality to the save button*/
+/* Adding functionality to the save button */
 save.addEventListener('click', function(){
     if(sname.value!="" && lastname.value!="" && email.value!="" && editing==false){
-        user.push({name:sname.value, lastname: lastname.value, email:email.value});
+        data.push({name:sname.value, lastName: lastname.value, email:email.value});
+        local.setItem('user', JSON.stringify(data));
     }else if(sname.value!="" && lastname.value!="" && email.value!="" && editing==true){
-        user[index].name=sname.value;
-        user[index].lastName=lastname.value;
-        user[index].email=email.value;
+        data[index].name=sname.value;
+        data[index].lastName=lastname.value;
+        data[index].email=email.value;
         editing=false;
+        local.setItem('user', JSON.stringify(data));
     }else{
         alert('All the fields must be populated');
     }
@@ -66,14 +79,15 @@ save.addEventListener('click', function(){
     rendertable();
 });
 
-/* Adding functionality to the edit and delete buttons*/
+/* Adding functionality to the edit and delete buttons */
 function loadbtn(){
     deleter=document.querySelectorAll('.delete');
     editor=document.querySelectorAll('.edit');
 
     for(let i=0; i<deleter.length; i++){
         deleter[i].addEventListener('click', function(){
-            user.splice(i,1);
+            data.splice(i,1);
+            local.setItem('user', JSON.stringify(data));
             clear();
             rendertable();
         });
@@ -81,13 +95,14 @@ function loadbtn(){
 
     for(let i=0; i<editor.length; i++){
         editor[i].addEventListener('click', function(){
-            sname.value=user[i].name;
-            lastname.value=user[i].lastName;
-            email.value=user[i].email;
+            sname.value=data[i].name;
+            lastname.value=data[i].lastName;
+            email.value=data[i].email;
             index=i;
             editing=true;
             clear();
             rendertable();
+            editor[i].parentNode.parentNode.setAttribute('class','editing');
         });
     }
 
